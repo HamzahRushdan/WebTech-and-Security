5e199adc8ea008cd7af0556cf903146166de1109
HamzahRushdan <112009697+HamzahRushdan@users.noreply.github.com>
2023-09-06 15:33:55 -0400

    Create TodoFooter.vue

---
+++ TodoFooter.vue
@@ -0,0 +1,49 @@
+<script>
+export default {
+    name: 'TodoFooter',
+
+    props: ['remaining', 'visibility', 'todos'],
+    emits: ['removeCompleted'],
+
+    data: () => ({}),
+
+    watch: {},
+
+    mounted() {
+
+    },
+
+    computed: {
+
+    },
+
+    methods: {
+        removeCompleted() {
+            this.$emit('removeCompleted');
+        }
+    }
+}
+</script>
+
+<template>
+    <footer class="footer" v-show="todos.length">
+        <span class="todo-count">
+            <strong>{{ remaining }}</strong>
+            <span>{{ remaining === 1 ? ' item' : ' items' }} left</span>
+        </span>
+        <ul class="filters">
+            <li>
+                <a href="#/all" :class="{ selected: visibility === 'all' }">All</a>
+            </li>
+            <li>
+                <a href="#/active" :class="{ selected: visibility === 'active' }">Active</a>
+            </li>
+            <li>
+                <a href="#/completed" :class="{ selected: visibility === 'completed' }">Completed</a>
+            </li>
+        </ul>
+        <button class="clear-completed" @click="removeCompleted" v-show="todos.length > remaining">
+            Clear completed todos
+        </button>
+    </footer>
+</template>
